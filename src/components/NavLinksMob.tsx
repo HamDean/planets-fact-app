126698cd113835b3c6d6a2d831b72d80e1026ea0
HamDean <hamdeen247@gmail.com>
2023-12-12 20:18:05 +0000

    Refactor - nav bar mobile

--- src/components/NavLinksMob.tsx
+++ src/components/NavLinksMob.tsx
@@ -2,8 +2,19 @@ import { planets } from "../data/constants";
 import { styles } from "../data/constants";
 
 const NavLinksMob = () => {
+  const colorCode: { [key: number]: string } = {
+    0: "#DEF4FC",
+    1: "#F7CC7F",
+    2: "#545BFE",
+    3: "#FF6A45",
+    4: "#ECAD7A",
+    5: "#FCCB6B",
+    6: "#65F0D5",
+    7: "#497EFA",
+  };
+
   return (
-    <ul className="flex flex-col h-[90vh] text-[15px] px-[24px] font-bold font-spartan bg-[#070724] uppercase tracking-[1.4px]">
+    <ul className="flex flex-col h-[90vh] md:hidden text-[15px] px-[24px] font-bold font-spartan bg-[#070724] uppercase tracking-[1.4px]">
       {planets.map((p, i) => (
         <li
           key={i}
@@ -11,7 +22,19 @@ const NavLinksMob = () => {
             i !== planets.length - 1 ? "border-b border-[#38384F]" : ""
           } py-10 ${styles.flexBetween}`}
         >
-          {p}
+          <span className={`${styles.flexBetween} gap-7`}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+              className="mb-2"
+            >
+              <circle cx="10" cy="10" r="10" fill={`${colorCode[i]}`} />
+            </svg>
+            {p}
+          </span>
           <img src="../src/assets/icon-chevron.svg" />
         </li>
       ))}
