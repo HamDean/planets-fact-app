1a1b99fa3b518ae85d9124a9a76cccea875caebb
Nurudeen Hamzah <hamdeen247@gmail.com>
2023-12-21 12:04:09 +0000

    Refactor - remove duplication of stat list

--- src/pages/PlanetPage.tsx
+++ src/pages/PlanetPage.tsx
@@ -1,11 +1,21 @@
 import { Outlet } from "react-router-dom";
 import FactTabMob from "../components/FactTabMob";
+import StatList from "../components/StatList";
+import planets from "../data/data";
+import usePlanetStore from "../store";
 
 const PlanetPage = () => {
+  const currentPlanet = usePlanetStore((s) => s.planet);
+
+  const planet = planets.find(
+    (planet) => planet.name.toLocaleLowerCase() === currentPlanet
+  );
+
   return (
     <>
       <FactTabMob />
       <Outlet />
+      <StatList planet={planet} />
     </>
   );
 };
