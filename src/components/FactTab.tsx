5028c7ad44928dac6ac1ef6cc9a2a563844b80fb
Nurudeen Hamzah <hamdeen247@gmail.com>
2023-12-25 07:48:24 +0000

    Implement active tab state

--- src/components/FactTab.tsx
+++ src/components/FactTab.tsx
@@ -1,15 +1,20 @@
 interface Props {
   index: number;
   title: string;
+  isActive: boolean;
+  onSelectTab: () => void;
 }
 
-const FactTab = ({ index, title }: Props) => {
+const FactTab = ({ index, title, isActive, onSelectTab }: Props) => {
   return (
     <div
       className={`flex justify-start items-center text-[9px] h-[48px] 
         w-[280px] lg:w-full gap-8 font-bold border-[#38384F] border 
-        px-[28px] py-[12px] mb-3 hover:bg-[#38384F] cursor-pointer 
+        px-[28px] py-[12px] mb-3 hover:bg-[#38384F] ${
+          isActive && "bg-green-500"
+        } cursor-pointer 
         transition-all`}
+      onClick={onSelectTab}
     >
       <h4 className=" opacity-[0.5]">0{index}</h4>
       <h3 className="uppercase">{title}</h3>
