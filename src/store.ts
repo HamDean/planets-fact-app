0de3add55830c5bc1679fb343632dd765c6fdf87
Nurudeen Hamzah <hamdeen247@gmail.com>
2023-12-25 14:18:09 +0000

    Implement active state for nav bar

--- src/store.ts
+++ src/store.ts
@@ -5,21 +5,25 @@ interface PlanetStore {
   tab: string;
   isOpen: boolean;
   selectedIndex: number;
+  planetIndex: number;
   setPlanet: (planet: string) => void;
   setTab: (tab: string) => void;
   setOpen: () => void;
   setSelectedIndex: (index: number) => void;
+  setPlanetIndex: (index: number) => void;
 }
 
 const usePlanetStore = create<PlanetStore>((set) => ({
   planet: "mercury",
   tab: "overview",
   isOpen: false,
-  selectedIndex: 1,
+  selectedIndex: 0,
+  planetIndex: 0,
   setPlanet: (planet) => set(() => ({ planet: planet })),
   setOpen: () => set((store) => ({ isOpen: !store.isOpen })),
   setTab: (tab) => set(() => ({ tab: tab })),
   setSelectedIndex: (index) => set(() => ({ selectedIndex: index })),
+  setPlanetIndex: (index) => set(() => ({ planetIndex: index })),
 }));
 
 export default usePlanetStore;
