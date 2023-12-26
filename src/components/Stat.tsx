277d717c3bd6cff2322ead5a759aa269cda36481
Nurudeen Hamzah <hamdeen247@gmail.com>
2023-12-26 21:58:04 +0000

    Refactor - adjust the page layout

--- src/components/Stat.tsx
+++ src/components/Stat.tsx
@@ -6,7 +6,7 @@ interface Props {
 const StatMob = ({ stat, figure }: Props) => {
   return (
     <div
-      className={`flex justify-between items-center min-w-[320px] md:min-w-[165px] md:flex-col md:items-start px-[10px]   md:p-5 py-[13px] border-[#38384F] border h-[48px] md:h-auto md:mb1  uppercase mb-[8px] md:mb-0  text-left`}
+      className={`flex justify-between items-center min-w-[320px] md:min-w-[165px] lg:w-[375px] md:flex-col md:items-start px-[10px]   md:p-5 py-[13px] border-[#38384F] border h-[48px] md:h-auto md:mb1  uppercase mb-[8px] md:mb-0  text-left`}
     >
       <p className="text-[#838391] text-[9px] font-bold font-spartan">{stat}</p>
       <span className="text-[16px] tracking-[-1px] md:text-[20px] font-antonio text-left ">
